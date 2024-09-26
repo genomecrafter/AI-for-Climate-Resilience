dea7d97fdf87e2e49c3be61592e7ed5850331d7e
Nikita S Raj Kapini <nikitasraj16@gmail.com>
2024-09-26 15:41:32 +0530

    Initial commit

---
+++ Tabs/about.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+def app():
+    st.title("TECH TITANS")
+
+    st.markdown('Team Members')
+    st.markdown('1. Dhruti Rudrangi,ISE, RVCE')
+    st.markdown('2. Nikita S Raj Kapini,CSE, RVCE')
+    st.markdown('3. Tulya Reddy Y,CSE, RVCE')
+
---
+++ Tabs/home.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+def app():
+
+    st.markdown(
+    """<h1 style="text-align:center;">
+            IEEE Sustain Fiesta 2024
+        </h1>
+        <h3 style="text-align:center;">
+            Young Pros Unite for Green Vibes
+        </h3>
+    """, unsafe_allow_html=True)
+
+
+    st.title("TECH TITANS presents: AI for Climate Resilience")
+
+    st.image("/images/earth.png")
+
+   
