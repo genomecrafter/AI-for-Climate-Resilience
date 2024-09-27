18e6b1fbee05048a5955ba27431ead3a3160ca9e
dhruthirudrangi <dhruthirudrangi@gmail.com>
2024-09-27 19:29:40 +0530

    modified dataframe

--- web_functions.py
+++ web_functions.py
@@ -3,3 +3,11 @@ import pandas as pd
 from prophet import Prophet
 from prophet.plot import plot_plotly, plot_components_plotly, plot_cross_validation_metric
 import streamlit as st
+
+
+df = pd.read_csv('weather.csv')
+
+threshold = len(df) * 0.9
+
+df.dropna(thresh=len(df) - threshold, axis=1, inplace=True)
+
